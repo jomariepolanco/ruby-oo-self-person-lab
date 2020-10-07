f6a60a07498c5d9b9c56d2379f9c474da2fea350
jomariepolanco <jomariepolanco@gmail.com>
2020-10-07 11:06:54 -0400

    oo person lab up to hygiene

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require 'pry'
+class Person
+
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+    def initialize(name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8 
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+    end
+
+end
+#binding.pry 
