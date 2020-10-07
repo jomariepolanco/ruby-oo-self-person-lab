77b00d5a935d6f826c71f0549aa60cf985dc06cd
jomariepolanco <jomariepolanco@gmail.com>
2020-10-07 19:51:16 -0400

    finished oo person lab done

--- lib/person.rb
+++ lib/person.rb
@@ -70,6 +70,21 @@ class Person
     def call_friend(friend)
         self.happiness += 3
         friend.happiness += 3 
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
-end
-        #binding.pry 
+
+    def start_conversation(person, topic)
+      if topic == "politics"
+        person.happiness -= 2
+        self.happiness -= 2
+        return "blah blah partisan blah lobbyist"
+      elsif topic == "weather"
+        person.happiness += 1
+        self.happiness += 1
+        return "blah blah sun blah rain"
+      else 
+        return "blah blah blah blah blah"
+      end
+    end
+end 
+       # binding.pry 
